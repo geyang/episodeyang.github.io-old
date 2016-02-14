6576bc48f58794857d61ed00301a77405e37708a
Ge Yang <yangge1987@gmail.com>
2016-02-14 14:53:31 -0600

    add animation

--- front-page/index.js
+++ front-page/index.js
@@ -2,36 +2,23 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
-    //app();
+    app();
 });
 
 function app() {
     var body = document.querySelector('body');
-    var contentPane = document.querySelector('.photo-pane');
-    var textPane = document.querySelector('.text-pane');
-    var secondImage = document.querySelector('.photo-pane>.full-pane:nth-child(2)');
-    var pageHeight = secondImage.clientHeight;
+    var fixedHero = document.querySelector('.fixed-hero');
+    var pageHeight = window.innerHeight;
 
-    var scrollThreshold = 0.2;
     function onScroll(scrollTarget) {
         return function () {
-            //console.log(contentPane.scrollTop);
-            console.log([event.target]);
-            if (scrollTarget.scrollTop < scrollThreshold * pageHeight) {
-                textPane.setAttribute('scroll', 'designer');
-                console.log('designer');
-            } else if (scrollTarget.scrollTop < (1 + scrollThreshold) * pageHeight) {
-                textPane.setAttribute('scroll', 'developer');
-                console.log('developer');
-            } else if (scrollTarget.scrollTop < (2 + scrollThreshold) * pageHeight) {
-                textPane.setAttribute('scroll', 'projects');
-                console.log('projects');
-            //} else if (scrollTarget.scrollTop > 2.95 * pageHeight) {
-            //    console.log('projects');
-            }
+            //console.log([event.target]);
+            var opacity = 1 -  Math.min(scrollTarget.scrollTop / pageHeight * 1, 1);
+            var shiftUp = (1 - opacity) * pageHeight * 0.3;
+            fixedHero.setAttribute('style', "opacity: " + opacity + "; top: " + (- shiftUp) + "px;" );
+            //console.log(opacity);
         }
     }
 
-    contentPane.addEventListener('scroll', onScroll(contentPane));
     document.addEventListener('scroll', onScroll(body));
 }
