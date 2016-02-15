1877a0d876541af6e89790d52c1eb70e08b7db40
Ge Yang <yangge1987@gmail.com>
2016-02-14 22:02:32 -0600

    got nav-bar to work perfectly

--- front-page/index.js
+++ front-page/index.js
@@ -8,18 +8,76 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function app() {
     var body = document.querySelector('body');
     var fixedHero = document.querySelector('.fixed-hero');
-    var pageHeight = window.innerHeight;
+    var navbarContent = document.querySelector('.fixed-navbar>.page-width-container');
+    var contentContainer = document.querySelector('.content-container');
+    var pageHeight;
 
     function onScroll(scrollTarget) {
-        return function () {
-            //console.log([event.target]);
+        var scrollRefractoryPeriod = null;
+
+        function scrollHandler() {
+            pageHeight = getWindowHeight();
             var scrollProgress = scrollTarget.scrollTop / pageHeight;
-            var opacity = 1 -  Math.min(scrollProgress * 1, 1);
+            var opacity = 1 - Math.min(scrollProgress * 1, 1);
             var shiftUp = (1 - opacity) * pageHeight * 0.3;
-            fixedHero.setAttribute('style', "opacity: " + opacity + "; top: " + (- shiftUp) + "px;" );
-            //console.log(opacity);
+
+            fixedHero.setAttribute('style', "opacity: " + opacity.toString().slice(0, 4) + "; top: " + ( -shiftUp) + "px;");
+            contentContainer.setAttribute('style', "opacity: " + scrollProgress + ";");
+            var navbarTop = Math.max(
+                -100,
+                -400 * (1 - Math.max(
+                    0,
+                    (Math.min(1, scrollProgress + 0.2)))
+                )
+            );
+            navbarContent.setAttribute('style', "top: " + navbarTop.toString().slice(0, 6) + "px;");
         }
+
+        return scrollHandler;
     }
 
     document.addEventListener('scroll', onScroll(body));
+
+    if (isMobileSafari()) {
+        fixHeight();
+        window.onorientationchange = fixHeight;
+    }
 }
+
+
+function isMobileSafari() {
+    return !!(window.navigator.userAgent.match(/iPhone|iPad|iPod/i));
+}
+function isLandscape() {
+    if (typeof window.orientation == 'undefined') return null;
+    else return (Math.abs(window.orientation) === 90)
+}
+
+function getWindowHeight() {
+    if (screen.height < window.innerHeight) {
+        if (isLandscape()) return screen.height;
+        else return screen.height - 108;
+    } else {
+        return window.innerHeight;
+    }
+}
+function fixHeight() {
+    var height = getWindowHeight();
+    //console.log(height);
+    //document.querySelector('h1').innerText = screen.height;
+    injectCss(".fixed-hero, .fixed-hero-shadow { height: " + height + "px; }");
+}
+
+/**
+ * returns the sheet object that we injected, so that it can be removed.
+ * @param css
+ * @param styleElement
+ * @returns {Element}
+ */
+function injectCss(css, styleElement) {
+    if (!styleElement) styleElement = document.createElement('style');
+    styleElement.innerHTML = css;
+    document.body.appendChild(styleElement);
+    return styleElement;
+}
+
